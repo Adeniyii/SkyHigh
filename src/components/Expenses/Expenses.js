f5939a87f6d57accd29a4b1faf4bf113dd358041
Adeniyii <ifedayoadeniyi@gmail.com>
2021-11-04 22:45:11 +0100

    :sparkles: [feat] filter expenses by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,27 +8,36 @@ function Expenses(prop) {
   const [filter, setFilter] = useState("2020");
 
   // Get lifted state from child
+  // set state to equal lifted state
   const onNewFilter = (filter) => {
-    // set state to equal lifted state
     setFilter(filter);
   };
 
-  console.log(filter);
+  const filteredExpenses = prop.expenseList.filter((exp) => {
+    return filter === exp.date.getFullYear().toString();
+  });
+
+  const displayedExpense =
+    filteredExpenses.length < 1 ? (
+      <p style={{ color: "#fff" }}>No expenses for {filter}.</p>
+    ) : (
+      filteredExpenses.map((exp) => {
+        return (
+          <ExpenseItem
+            key={exp.id}
+            title={exp.title}
+            amount={exp.amount}
+            date={exp.date}
+          ></ExpenseItem>
+        );
+      })
+    );
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter onNewFilter={onNewFilter} filterValue={filter} />
-        {prop.expenseList.map((exp) => {
-          return (
-            <ExpenseItem
-              key={exp.id}
-              title={exp.title}
-              amount={exp.amount}
-              date={exp.date}
-            ></ExpenseItem>
-          );
-        })}
+        {displayedExpense}
       </Card>
     </div>
   );
